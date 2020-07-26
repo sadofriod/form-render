2033e054ac392bc9302fcf7727db5a178b84cb6d
ashes <792634614@qq.com>
2020-07-26 13:34:38 +0000

    update from docker

--- src/App.tsx
+++ src/App.tsx
@@ -35,46 +35,75 @@ import NewFormItemDialog from "./component/editor/newFormItemDialog";
 // };
 
 function App() {
-	const [state, setState] = useState<any>();
-	const [dic, setDic] = useState<IDictionary[]>(dictionary);
-	const [isOpen, setOpen] = useState(false);
-	const [path, setPath] = useState(".root");
-	const [componentType, setComponentType] = useState<keyof IDictionaryType>();
+  const [state, setState] = useState<any>();
+  const [dic, setDic] = useState<IDictionary[]>(dictionary);
+  const [isOpen, setOpen] = useState(false);
+  const [path, setPath] = useState(".root");
+  const [componentType, setComponentType] = useState<keyof IDictionaryType>();
 
-	return (
-		<div className="App">
-			<div className="left">
-				<h3>可选表单组件</h3>
-				<Editor
-					setDic={setDic}
-					createNewFormItem={(val: keyof IDictionaryType) => {
-						setOpen(true);
-						setComponentType(val);
-					}}
-				/>
-			</div>
-			<div className="center">
-				<h3>表单结构预览</h3>
-				<h4 style={{ textAlign: "left" }}>当前路径：{path}</h4>
-				<Core
-					setPath={(val: string) => {
-						setPath(val);
-					}}
-					onChange={setState}
-					dictionary={dic}
-				/>
-			</div>
-			<div className="right">
-				<h3>结果集</h3>
-				<pre suppressContentEditableWarning={true} contentEditable={true} style={{ overflow: "auto", height: "40%", width: "100%", textAlign: "left", padding: "5px", border: "2px solid #000" }}>
-					{JSON.stringify(state, null, 2)}
-				</pre>
-				<h3>字典</h3>
-				<pre style={{ overflow: "auto", height: "40%", width: "100%", textAlign: "left", padding: "5px", border: "2px solid #000" }}>{JSON.stringify(state, null, 2)}</pre>
-			</div>
-			<NewFormItemDialog compType={componentType} setOpen={setOpen} dictionary={dic} isOpen={isOpen} setDic={setDic} path={path} />
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="left">
+        <h3>可选表单组件</h3>
+        <Editor
+          setDic={setDic}
+          createNewFormItem={(val: keyof IDictionaryType) => {
+            setOpen(true);
+            setComponentType(val);
+          }}
+        />
+      </div>
+      <div className="center">
+        <h3>表单结构预览</h3>
+        <h4 style={{ textAlign: "left" }}>当前路径：{path}</h4>
+        <Core
+          setPath={(val: string) => {
+            setPath(val);
+          }}
+          onChange={setState}
+          dictionary={dic}
+        />
+      </div>
+      <div className="right">
+        <h3>结果集</h3>
+        <pre
+          suppressContentEditableWarning={true}
+          contentEditable={true}
+          style={{
+            overflow: "auto",
+            height: "40%",
+            width: "100%",
+            textAlign: "left",
+            padding: "5px",
+            border: "2px solid #000",
+          }}
+        >
+          {JSON.stringify(state, null, 2)}
+        </pre>
+        <h3>字典</h3>
+        <pre
+          style={{
+            overflow: "auto",
+            height: "40%",
+            width: "100%",
+            textAlign: "left",
+            padding: "5px",
+            border: "2px solid #000",
+          }}
+        >
+          {JSON.stringify(dic, null, 2)}
+        </pre>
+      </div>
+      <NewFormItemDialog
+        compType={componentType}
+        setOpen={setOpen}
+        dictionary={dic}
+        isOpen={isOpen}
+        setDic={setDic}
+        path={path}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/util/setValueByModel.ts
+++ src/util/setValueByModel.ts
@@ -3,22 +3,22 @@ import { produce } from "immer";
 import formatPath from "./formatPath";
 
 const setValueByModel = (obj: any, path: string, value: any) => {
-	const realPath = formatPath(path).split(".");
+  const realPath = formatPath(path).split(".");
 
-	return produce(obj, (obj: any) => {
-		realPath.forEach((item, index) => {
-			if (obj && item in obj) {
-				if (!item) {
-					return;
-				}
-				if (index === realPath.length - 1) {
-					obj[item] = value;
-				} else {
-					obj = obj[item];
-				}
-			}
-		});
-	});
+  return produce(obj, (obj: any) => {
+    realPath.forEach((item, index) => {
+      if (obj && item in obj) {
+        if (!item) {
+          return;
+        }
+        if (index === realPath.length - 1) {
+          obj[item] = value;
+        } else {
+          obj = obj[item];
+        }
+      }
+    });
+  });
 };
 
 export default setValueByModel;
